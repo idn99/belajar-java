50c761277ed7995ad0a1f2333787156dd2de541d
Ilham Dwi Nugraha <ilhamdn99@gmail.com>
2019-12-03 09:47:54 +0700

    menambah looping for

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld{
     public static void main(String args[]){
-        System.out.println("Hallo Dunia");
+        for(int i=0;i<=10;i++){
+            System.out.println("Hallo Dunia");
+        }
     }
 }
