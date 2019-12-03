7b04502d5e014c0f4f3c183af466420700641bb5
idn99 <43363201+idn99@users.noreply.github.com>
2019-12-03 10:14:46 +0700

    menambahkan looping while

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,8 @@ public class helloworld{
         for(int i=0;i<=10;i++){
             System.out.println("Hallo Dunia");
         }
+        while(int a<=9){
+            System.out.println("How are you today ? :D");
+        }
     }
 }
